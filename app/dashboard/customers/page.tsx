bfe0d841d6bb9c8b28e6d4e97f26f90c962c7f01
KhaledSaeed18 <khaled18saeed@gmail.com>
2025-02-02 21:46:31 +0200

    Chapter 16

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,4 +1,10 @@
+import { Metadata } from 'next';
 import React from 'react'
+import { Suspense } from 'react';
+
+export const metadata: Metadata = {
+    title: 'Customers',
+};
 
 const page = () => {
     return (
